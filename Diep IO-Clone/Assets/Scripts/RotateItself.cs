1803dbd7ff5744ed37426e9f44b97786ce0deffc
Sena Karadeniz <senaakaradenizz@gmail.com>
2018-01-12 18:10:18 +0300

    Creare RotateItself.cs file and implement it. Change Polygon, rectangle, triangle prefabs.

---
+++ Diep IO-Clone/Assets/Scripts/RotateItself.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateItself : MonoBehaviour {
+
+    [Range(1,100)]
+    public float speed;
+
+	void Start () {
+        speed = Random.Range(1f, 100f);
+
+        if (Random.Range(0f, 1f) < 0.5f)
+            speed = -speed; //Clockwise or Counter clockwise.
+	}
+	
+	void Update () {
+        RotateIt();
+	}
+
+    void RotateIt ()
+    {
+        transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+    }
+}
