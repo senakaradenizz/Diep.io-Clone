3e25783cc01bec80ef8158bbebbf129ea7c89ab8
Sena Karadeniz <senaakaradenizz@gmail.com>
2018-01-12 19:37:39 +0300

    Add map function for map the Health Bar depends on MAX_HEALTH const variable.

--- Diep IO-Clone/Assets/Scripts/PlayerController.cs
+++ Diep IO-Clone/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerController : MonoBehaviour {
+
+    private const float MAX_HEALTH = 100f;
 
     public float maxSpeed = 25f;
 
@@ -21,6 +24,7 @@ public class PlayerController : MonoBehaviour {
 
             if (_health <= 0)
             {
+                _health = 0;
                 //Do something about Die!
                 Debug.Log("You died!");
             }
@@ -36,12 +40,19 @@ public class PlayerController : MonoBehaviour {
     {
         healthGraphic = transform.Find("Health");
 
-        Health = 100;
+        Health = MAX_HEALTH;
     }
 
     void SetHealthGraphics()
     {
-        healthGraphic.localScale = new Vector3(Health / 100, 1, 1);
+        float mappedValue = Map(Health, 0, MAX_HEALTH, 0, 1);
+
+        healthGraphic.localScale = new Vector3(mappedValue, 1, 1);
+    }
+
+    float Map(float value, float low1, float high1, float low2, float high2)
+    {
+        return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
     }
 
     void Update()
